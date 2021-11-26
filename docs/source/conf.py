0f89c8841523a641bf61cb7374e83f6313803c9f
bbr <boris@zuckersalzundpfeffer.de>
2021-11-26 16:45:11 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'wagtail-tenants'
+copyright = '2021, Boris Brünen'
+author = 'Boris Brünen'
 
 release = '0.1'
 version = '0.1.0'

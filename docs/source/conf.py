6c5d813eabe802860d2f133b31afbae3e24d7a69
Boris Brue <boris@zuckersalzundpfeffer.de>
2021-12-03 14:42:21 +0100

    myst parser added to use md in docs with sphinx

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,8 +17,8 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
+    'myst_parser'
 ]
-
 intersphinx_mapping = {
     'python': ('https://docs.python.org/3/', None),
     'sphinx': ('https://www.sphinx-doc.org/en/master/', None),

01c646f984914be4aad08d7c27db0bce3f794a0a
Boris Brue <boris@zuckersalzundpfeffer.de>
2023-03-22 15:20:23 +0100

    update python version for readthedocs build

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,7 +17,7 @@ extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.autosummary",
     "sphinx.ext.intersphinx",
-    "myst-parser",
+    "myst_parser",
 ]
 intersphinx_mapping = {
     "python": ("https://docs.python.org/3/", None),
